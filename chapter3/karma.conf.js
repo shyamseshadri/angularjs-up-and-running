1693a49e7a8a028e9605782b6e1f98f25c28159a
Shyam Seshadri <shyamseshadri@gmail.com>
2014-05-23 22:40:09 +0530

    All examples for chapter 3

---
+++ chapter3/karma.conf.js
@@ -0,0 +1,50 @@
+// File:  chapter3/karma.conf.js
+// Karma configuration
+
+module.exports = function(config) {
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+    // testing framework to use (jasmine/mocha/qunit/...)
+    frameworks: ['jasmine'],
+
+    // list of files / patterns to load in the browser
+    files: [
+      'angular.min.js',
+      'angular-mocks.js',
+      'controller.js',
+      'simpleSpec.js',
+      'controllerSpec.js'
+    ],
+
+    // list of files / patterns to exclude
+    exclude: [],
+
+    // web server port
+    port: 8080,
+
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
---
+++ chapter3/simpleSpec.js
@@ -0,0 +1,21 @@
+// A Test Suite in Jasmine
+describe('My Function', function() {
+
+  var t;
+  // Similar to setup
+  beforeEach(function() {
+    t = true;
+  });
+
+  afterEach(function() {
+    t = null;
+  });
+
+  it('should perform action 1', function() {
+    expect(t).toBeTruthy();
+  });
+  it('should perform action 2', function() {
+    var expectedValue = true;
+    expect(t).toEqual(expectedValue);
+  });
+});
